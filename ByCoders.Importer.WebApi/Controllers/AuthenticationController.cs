19dd0bff41806f4f006495da9f9267760ab68e73
Ricardo Zambon <ricardo.zambon@live.com>
2022-04-02 19:45:44 -0300

    docs: created documentation for Swagger on how to use the api

--- ByCoders.Importer.WebApi/Controllers/AuthenticationController.cs
+++ ByCoders.Importer.WebApi/Controllers/AuthenticationController.cs
@@ -16,7 +16,24 @@ namespace ByCoders.Importer.WebApi.Controllers
             this.authenticationService = authenticationService;
         }
 
-
+        /// <summary>
+        /// Checks a username and password to grant access to the API.
+        /// </summary>
+        /// <param name="model">Username and password to authenticate.</param>
+        /// <returns>The JWT Token and Refresh Token.</returns>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /SignIn
+        ///     {
+        ///         "Username": "ricardo.zambon",
+        ///         "Password": "zambon"
+        ///     }
+        ///     
+        /// </remarks>
+        /// <response code="200">Sucessfully authenticated.</response>
+        /// <response code="401">Invalid username or password.</response>
+        /// <response code="400">Internal server issue.</response>
         [HttpPost, Route("[action]"), AllowAnonymous]
         public async Task<IActionResult> SignIn(SignInModel model)
         {
@@ -34,6 +51,14 @@ namespace ByCoders.Importer.WebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Refresh the JWT token using a valid Refresh Token.
+        /// </summary>
+        /// <param name="model">Username and password to authenticate.</param>
+        /// <returns>The JWT Token and Refresh Token.</returns>
+        /// <response code="200">Sucessfully refreshed the token.</response>
+        /// <response code="401">Invalid Refresh Token or Refresh Token expired.</response>
+        /// <response code="400">Internal server issue.</response>
         [HttpPost, Route("[action]"), AllowAnonymous]
         public async Task<IActionResult> RefreshToken(RefreshTokenModel model)
         {
