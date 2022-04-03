84f3db1bbd2f747f75ef0d16310cda332c63e632
Ricardo Zambon <ricardo.zambon@live.com>
2022-04-02 22:43:46 -0300

    refact: improved how number, value and datetime are formated in operations list

--- ByCoders.Importer.Angular/src/app/shared/grid-formatters.ts
+++ ByCoders.Importer.Angular/src/app/shared/grid-formatters.ts
@@ -1,14 +1,19 @@
 import { ValueFormatterParams } from 'ag-grid-community';
 
-export function currencyFormatter(params: ValueFormatterParams) {
-  const valueFormatter = new Intl.NumberFormat('en-US', { style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2 });
+export class DefaultFormatters {
+  public static numberFormatter = new Intl.NumberFormat('en-US', { style: 'decimal', minimumFractionDigits: 0, maximumFractionDigits: 0 });
   
+  public static valueFormatter = new Intl.NumberFormat('en-US', { style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  
+  public static dateTimeFormatter = Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false } )
+}
+
+export function currencyFormatter(params: ValueFormatterParams) {
   const number = parseFloat(params.value);
   
   if (!isNaN(number)) {
-    return valueFormatter.format(number);
+    return DefaultFormatters.valueFormatter.format(number);
   }
-
   return '';
 }
 
@@ -22,17 +27,7 @@ export function cpfFormatter(params: ValueFormatterParams) {
 
 export function dateTimeFormatter(params: ValueFormatterParams) {
   if (params.value) {
-    const date = new Date(params.value);
-
-    const year = date.getUTCFullYear();
-    const month = ('0' + (date.getUTCMonth()+1)).slice(-2);
-    const day = ('0' + date.getUTCDate()).slice(-2);
-
-    const hour = ('0' + date.getUTCHours()).slice(-2);
-    const minute = ('0' + date.getUTCMinutes()).slice(-2);
-    const second = ('0' + date.getUTCSeconds()).slice(-2);
-
-    return `${day}/${month}/${year} ${hour}:${minute}:${second}`
+    return DefaultFormatters.dateTimeFormatter.format(new Date(params.value));
   }
   return '';
 }
